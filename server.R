c1bff92e8d74fbd908d8e806592d31c0984e8c05
John Hamilton Bradford <john.bradford.mvsu@gmail.com>
2016-01-18 00:31:54 -0500

    Change Interface

--- server.R
+++ server.R
@@ -4,17 +4,21 @@ library(ggthemes)
 library(dplyr)
 library(tidyr)
 #devtools::install_github("jrnold/ggthemes")
-library(choroplethr)
-library(choroplethrMaps)
-library(RColorBrewer)
+# library(choroplethr)
+# library(choroplethrMaps)
+# library(RColorBrewer)
 library(Cairo)   # For nicer ggplot2 output when deployed on Linux
 
-data(state.map)
-data(county.map)
+# data(state.map)
+# data(county.map)
 cn <- read.csv("countyCrimeData.csv", na.strings=c("", "NA"), stringsAsFactors = F )
-
+# cn$showName <- FALSE
 
 shinyServer(function(input, output) {
+
+  ranges <- reactiveValues(x = NULL, y = NULL)
+  
+  # For storing which rows have been excluded
 
 
   datasetInput <-  reactive({
@@ -47,7 +51,17 @@ shinyServer(function(input, output) {
     
     STATE <- cn$State
     
-    cbind.data.frame(DV, IV, SIZE, POP, NAME, STATE, stringsAsFactors = F)
+    region <- cn$region
+    
+    df <- cbind.data.frame(NAME, STATE, DV, IV, POP, SIZE, region, stringsAsFactors = F) %>% 
+   #df <- cbind.data.frame(DV, IV, SIZE, POP, NAME, STATE, stringsAsFactors = F) %>% 
+     arrange(desc(SIZE))
+   if (input$st != "Show All") 
+   {df <- df %>% 
+     filter(STATE == input$st)
+   }
+   df
+
     })
     
 
@@ -58,46 +72,117 @@ shinyServer(function(input, output) {
 #   })
 #   
   
-  output$distPlot <- renderPlot({
-
-    df <- arrange(datasetInput(), desc(SIZE))
+#   vals <- reactiveValues(
+#     showing = rep(TRUE, nrow(cn))
+#   )
+  
+  output$plot1 <- renderPlot({
+    df <- datasetInput()
+   # df <- arrange(datasetInput(), desc(SIZE))
     n <- input$nlabels
     colorVar <- df$STATE
-    if (input$st != "Show All") 
-      {df <- df %>% 
-      filter(STATE == input$st)
-      colorVar <- df$SIZE
-      }
+    if (input$st != "Show All") {colorVar <- df$SIZE}
+    
+    
+    
     topList <- df[1:n,]
+      #df[!vals$keeprows, , drop = FALSE]
+     
 
     
-  thePlot <- ggplot(data=df, aes(y=DV, x=IV))
+    # Plot the kept and excluded points as two separate data sets
+    #keep    <- df[ vals$keeprows, , drop = FALSE]
+    #showName <- df[!vals$keeprows, , drop = FALSE]
+    
+    
+    
+    thePlot <- ggplot(data=df, aes(y=DV, x=IV)) +
+      geom_point(aes(size=SIZE, color=colorVar, alpha=SIZE))
 
-    Model <- lm(df$DV ~ df$IV, weights = df$POP)
-    S <- summary(Model)
+
+    
+    if(input$regL == "Yes"){
+      Model <- lm(df$DV ~ df$IV, weights = df$POP)
+      S <- summary(Model)
+      thePlot <- thePlot +    
+        geom_abline(intercept=Model$coefficients[1], 
+                    slope= Model$coefficients[2], color="red") +
+        ggtitle(paste("Linear Regression of ", input$dv, " on ", input$iv, " for US Counties", 
+                      "\nWeighted by Population.  ", "R-squared: ", round(S$r.squared, 2), "  Coeff.:  ", 
+                      round(S$coefficients[2], 2), sep="")) 
+      
+      }
     
     plotOpts <- theme(axis.title.y = element_text(angle=90)) 
 
-      
     colorLegend <- if (input$st != "Show All") {scale_colour_continuous(guide = FALSE) }
     else{scale_colour_discrete(guide = FALSE)}
     
-    
 
-    thePlot + geom_point(aes(size=SIZE, color=colorVar)) +
-   geom_abline(intercept=Model$coefficients[1], slope= Model$coefficients[2], color="red") +
-    geom_text(data=topList, aes(x=IV, y=DV, label=NAME, size=SIZE), vjust=1) +
+    thePlot  +
+    geom_text(data=topList, aes(x=IV, y=DV, label=NAME), size=4, vjust=1) +
       ylab(label=input$dv) + xlab(label=input$iv) + 
-      ggtitle(paste("Linear Regression of ", input$dv, " on ", input$iv, " for US Counties", 
-                    "\nWeighted by Population.  ", "R-squared: ", round(S$r.squared, 2), "  Coeff.:  ", 
-                    round(S$coefficients[2], 2), sep="")) + scale_size_continuous(guide = FALSE) +
+      scale_size_continuous(guide = FALSE) +
+      scale_alpha_continuous(guide = FALSE) +
       colorLegend +
       plotOpts +
-      theme_gdocs() 
+      theme_gdocs() +
+      coord_cartesian(xlim = ranges$x, ylim = ranges$y)
 
     
-  
+#     #MAPS
+#     df$value <- df$SIZE
+#     myPalette <- colorRampPalette(rev(brewer.pal(5, "Spectral")))
+#     sc <- scale_fill_gradientn(colours = myPalette(100), limits=c(0,.65))
+#     t <-  theme(text=element_text(size=14),
+#                 plot.title = element_text(size=18), axis.title.x= element_text(size=12)) 
+#     
+#     county_choropleth(df, 
+#                       num_colors=1) + theme_gdocs() + t + sc
+#   
     
   })
+  
+  # Toggle points that are clicked
+#   observeEvent(input$plot1_click, {
+#     res <- nearPoints(df, input$plot1_click, allRows = TRUE)
+#     
+#     vals$keeprows <- xor(vals$keeprows, res$selected_)
+#   })
+  
+  # When a double-click happens, check if there's a brush on the plot.
+  # If so, zoom to the brush bounds; if not, reset the zoom.
+  observeEvent(input$plot1_dblclick, {
+    brush <- input$plot1_brush
+    if (!is.null(brush)) {
+      ranges$x <- c(brush$xmin, brush$xmax)
+      ranges$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges$x <- NULL
+      ranges$y <- NULL
+    }
+  })
+  
+  output$click_info <- renderPrint({
+    df <- datasetInput()
+    df <- df[,1:5]
+    names(df) <- c("County", "State", input$dv, input$iv, "Pop") # input$scale, input$scale )
+    #df <- df[,-6]
+#     new_df <- df
+#     new_df[,1] <- df$County
+#     new_df[,2] <- df$State
+#     new_df[,3] <- df[,1]
+#     new_df[,4] <- df[,2]
+    varNames <- names(df)
+    # Because it's a ggplot2, we don't need to supply xvar or yvar; if this
+    # were a base graphics plot, we'd need those.
+    nearPoints(df=df, xvar=varNames[4], yvar=varNames[3], input$plot1_click, addDist = TRUE)
+  })
+  
+#   output$hover_info <- renderPrint({
+#     cat("input$plot_hover:\n")
+#     str(input$plot_hover)
+#   })
   
 })
--- ui.R
+++ ui.R
@@ -3,13 +3,14 @@ library(ggplot2)
 library(ggthemes)
 library(dplyr)
 library(tidyr)
-library(choroplethr)
-library(choroplethrMaps)
-library(RColorBrewer)
+# library(choroplethr)
+# library(choroplethrMaps)
+# library(RColorBrewer)
 library(Cairo)   # For nicer ggplot2 output when deployed on Linux
-
+cn <- read.csv("countyCrimeData.csv", na.strings=c("", "NA"), stringsAsFactors = F )
 stateList <- unique(cn$State)
 shinyUI(fluidPage(
+
   # Application title
   titlePanel("County Level Crime Data Regression"),
 
@@ -18,9 +19,10 @@ shinyUI(fluidPage(
     sidebarPanel(
       p("by John H. Bradford"),
       p("Data from countyhealthrankings.org"),
+      radioButtons("regL", label="Add Fitted Line?", choices=c("Yes", "No"), selected="No"),
       selectInput("st", label="Filter by State", c("Show All", stateList), selected="Show All"),
-      selectInput("dv", label="Dependent Variable", c("Homicides","Violent Crime"), selected="Homicides"),
-      selectInput("iv", label="Independent Variable", 
+      selectInput("dv", label="Y-axis - Dependent Variable", c("Homicides","Violent Crime"), selected="Homicides"),
+      selectInput("iv", label="X-axis - Independent Variable", 
                   c("High School Graduation","Unemployment", "Child Poverty",
                     "Income Inequality", "Median Income", "Single Parent Households", "Social Associations",
                     "Black Pop.", "Hispanic Pop.", "White Pop.", "Female Pop.", 
@@ -35,8 +37,19 @@ shinyUI(fluidPage(
       ),
     # Show a plot of the generated distribution
     mainPanel(
-      #verbatimTextOutput("John Bradford"),
-      plotOutput("distPlot")
+      h6("Select area and double-click to zoom.  Double-click to zoom out.  Click on data point to see more info."),
+      plotOutput("plot1",
+#                  hover = hoverOpts(
+#                    id = "plot_hover"),
+                 click = "plot1_click",
+                 dblclick = "plot1_dblclick",
+                 brush = brushOpts(
+                   id = "plot1_brush",
+                   resetOnNew = TRUE
+                 )),
+        verbatimTextOutput("click_info")
+      #verbatimTextOutput("hover_info")
+      
     )
   )
 ))
